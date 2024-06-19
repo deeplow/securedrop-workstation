acd8fefdd0178b8bc21272d6a9328d1ea8c06ab3
deeplow <francisco@freedom.press>
2024-06-19 10:34:01 -0400

    WIP

--- tests/base.py
+++ tests/base.py
@@ -15,9 +15,13 @@ SD_VMS = ["sd-gpg", "sd-log", "sd-proxy", "sd-app", "sd-viewer", "sd-whonix", "s
 SD_DVM_TEMPLATES = ["sd-devices-dvm", "sd-proxy-dvm"]
 SD_TEMPLATES = [SD_TEMPLATE_BASE, SD_TEMPLATE_LARGE, SD_TEMPLATE_SMALL]
 
-CURRENT_FEDORA_VERSION = "40"
-CURRENT_FEDORA_TEMPLATE = "fedora-" + CURRENT_FEDORA_VERSION + "-xfce"
-CURRENT_FEDORA_DVM = "fedora-" + CURRENT_FEDORA_VERSION + "-dvm"
+fedora_template_fmt = "fedora-{}-xfce"
+fedora_dvm_fmt = "fedora-{}-dvm"
+CURRENT_FEDORA_VERSION = 40
+CURRENT_FEDORA_TEMPLATE = fedora_template_fmt.format(CURRENT_FEDORA_VERSION)
+CURRENT_FEDORA_DVM = fedora_dvm_fmt.format(CURRENT_FEDORA_VERSION)
+PREVIOUS_FEDORA_TEMPLATE = fedora_template_fmt.format(CURRENT_FEDORA_VERSION - 1)
+PREVIOUS_FEDORA_DVM = fedora_dvm_fmt.format(CURRENT_FEDORA_VERSION - 1)
 CURRENT_WHONIX_VERSION = "17"
 
 
